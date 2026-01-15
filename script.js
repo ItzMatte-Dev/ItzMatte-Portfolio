3e3d6848a52ba51a223b7f67ac21738fb04e2369
ItzMatte-Dev <mateusz.ogorek3009@gmail.com>
2026-01-15 15:11:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
